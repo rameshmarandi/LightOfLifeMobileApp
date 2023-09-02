d3d2e062685914ed50c039c6a5b3792ed65e7e02
ramesh.marandi@aiab.in <ramesh.marandi@aiab.in>
2023-09-03 01:52:07 +0530

    changes one the event apge where i have added the Event UI

--- src/Screens/Client/Events/index.js
+++ src/Screens/Client/Events/index.js
@@ -44,6 +44,7 @@ import {
   SCREENHEIGHT,
   getFontSize,
   hp,
+  getResWidth,
   wp,
   getResHeight,
 } from '../../../utility/responsive';
@@ -53,6 +54,7 @@ import {Button} from 'react-native-elements';
 import GoogleMapComp from '../../../components/GoogleMapComp';
 import QuickRouteComp from '../../../components/QuickRouteComp';
 import {
+  CommonButton,
   CommonButtonComp,
   CommonModal,
   DropdownComp,
@@ -66,7 +68,7 @@ const TabsNames = [
   {key: '1', tabTitle: 'Past Events'},
 ];
 
-const Events = React.memo(props => {
+const Events = props => {
   const [activeIndex, setActiveIndex] = useState(0);
 
   return (
@@ -82,7 +84,6 @@ const Events = React.memo(props => {
               props.navigation.goBack();
             }}
             screenTitle={'All Events'}
-            // centerLogo={true}
           />
         </View>
         <View
@@ -114,9 +115,12 @@ const Events = React.memo(props => {
                   justifyContent: 'center',
                   backgroundColor: backgroundColorHandler(),
                   borderBottomWidth: activeIndex === index ? 2 : 2,
-                  borderBottomColor: activeIndex === index ?theme.color.error :'#C0C0C0',
+                  borderBottomColor:
+                    activeIndex === index ? theme.color.error : '#C0C0C0',
                   marginBottom: '3.5%',
                   paddingBottom: 0,
+                  transitionProperty: 'borderBottomColor',
+                  transitionDuration: '0.9s',
                 },
               ]}
               buttonStyle={[
@@ -130,12 +134,142 @@ const Events = React.memo(props => {
             />
           ))}
         </View>
-        <View>
+        <View
+          style={{
+            paddingHorizontal: '5%',
+            flex: 1,
+          }}>
+          <FlatList
+            data={[1, 1, 1, 1, 1, 1, 1, 1, 1]}
+            showsVerticalScrollIndicator={false}
+            renderItem={({item, index}) => {
+              return (
+                <>
+                  <View
+                    style={{
+                      width: '98%',
+                      padding: '5%',
+                      borderWidth: 1,
+                      backgroundColor: '#FFFFFF',
+                      shadowColor: 'black',
+                      shadowOffset: {width: 0, height: 1},
+                      shadowRadius: 10,
+                      shadowOpacity: 0.1,
+                      elevation: 3,
+                      borderRadius: 10,
+                      borderColor: '#F3EEF2',
+                      marginBottom: '5%',
+                      margin: 5,
+                      overflow: 'hidden',
+                    }}>
+                    <View
+                      style={{
+                        flexDirection: 'row',
+                        // justifyContent:"center",
+                        alignItems:"center"
+                      }}>
+                      <Image
+                        source={{
+                          uri: 'https://img.freepik.com/premium-vector/upcoming-events-speech-bubble-banner-with-upcoming-events-text-glassmorphism-style-business-marketing-advertising-vector-isolated-background-eps-10_399089-2079.jpg',
+                        }}
+                        resizeMode={'contain'}
+                        style={{
+                          width: 80,
+                          height: 80,
+                          backgroundColor: 'green',
+                          borderRadius: 10,
+                        }}
+                      />
+                      <View
+                        style={{
+                          marginLeft: '4%',
+                        }}>
+                        <Text>Birth day</Text>
+                        <Text>Location : Pimple Guruv</Text>
+                      </View>
+                    </View>
+                    <View
+                      style={{
+                        width: '100%',
+                        alignSelf: 'center',
+                        marginTop: '5%',
+                      }}>
+                      <Button
+                        title={'Join us'}
+                        onPress={() => {}}
+                        disabledStyle={{
+                          backgroundColor: theme.color.disabledBtn,
+                        }}
+                        disabledTitleStyle={{
+                          color: theme.color.accent,
+                          fontSize: getFontSize(16),
+                          fontFamily: theme.font.semiBold,
+                          fontWeight: 600,
+                        }}
+                        titleStyle={[
+                          {
+                            color: 'white',
+                            fontSize: getFontSize(12),
+                            fontFamily: theme.font.semiBold,
+                            fontWeight: 600,
+                          },
+                        ]}
+                        loadingProps={{size: 'small', color: 'white'}}
+                        containerStyle={[
+                          {
+                            width: '100%',
+                            height: getResHeight(35),
+                          },
+                        ]}
+                        buttonStyle={[
+                          {
+                            width: '100%',
+                            height: '100%',
+                            borderRadius: getResWidth(8),
+                            backgroundColor: theme.color.seletedBtn,
+                          },
+                        ]}
+                        letfIcon
+                      />
+                    </View>
+                    <View
+                      style={{
+                        position: 'absolute',
+                        right: -5,
+                        top: -1,
+                        paddingHorizontal: '5%',
+                        backgroundColor: '#D74826',
+                        alignItems: 'center',
+                        borderBottomLeftRadius: 10,
+                      }}>
+                      <Text
+                        style={{
+                          fontFamily: theme.font.bold,
+                          color: theme.color.white,
+                          fontSize: getFontSize(19),
+                        }}>
+                        6
+                      </Text>
+                      <Text
+                        style={{
+                          fontFamily: theme.font.bold,
+                          color: theme.color.white,
+                          fontSize: getFontSize(11),
+                        }}>
+                        Aug
+                      </Text>
+                    </View>
+                  </View>
+                </>
+              );
+            }}
+          />
+
           <Text>All Events</Text>
         </View>
       </SafeAreaView>
     </>
   );
-});
+};
 
 export default Events;
